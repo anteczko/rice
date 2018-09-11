cce3e3dec56c4073f9181fce50beb2afdbad5f39
anteczko <s21@onet.pl>
2018-09-11 12:47:52 +0200

    third stage: i3-gaps auto installer

--- script.sh
+++ script.sh
@@ -1,3 +1,27 @@
 #!/bin/bash
-echo pi!
+echo installing basic command line programs
 
+sudo apt-get install feh htop vim i3 autoconf xcb-xrm
+sudo apt install libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf xutils-dev libtool 
+sudo add-apt-repository ppa:aguignard/ppa
+sudo apt-get update
+sudo apt-get install libxcb-xrm-dev
+
+
+echo now you need to logout and run i3
+
+mkdir gaps
+cd ./gaps
+git clone https://www.github.com/Airblader/i3 i3-gaps
+cd i3-gaps
+
+# compile & install
+autoreconf --force --install
+rm -rf build/
+mkdir -p build && cd build/
+
+# Disabling sanitizers is important for release versions!
+ The prefix and sysconfdir are, obviously, dependent on the distribution.
+../configure --prefix=/usr --sysconfdir=/etc --disable-sanitizers
+make
+sudo make install
