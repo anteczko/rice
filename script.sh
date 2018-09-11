8fe8f440665f7579fc1b92afda9eef3df059c326
anteczko <s21@onet.pl>
2018-09-11 21:53:19 +0200

    Now with compton installer!

--- script.sh
+++ script.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-echo installing basic command line programs
-
+<< ////
+echo installing i3
 sudo apt-get install feh htop vim i3 autoconf xcb-xrm
 sudo apt install libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf xutils-dev libtool 
 sudo add-apt-repository ppa:aguignard/ppa
 sudo apt-get update
 sudo apt-get install libxcb-xrm-dev
-
+sudo apt-get update -y
 
 echo now you need to logout and run i3
 
@@ -32,3 +32,19 @@ sudo echo "for_window [class="^.*"] border pixel 2" >> ~/.config/i3/config
 i3-msg restart
 
 rm -rf ./gaps
+sudo apt-get update -y
+////
+echo installed i3-gaps and reloaded
+
+sudo apt-get install apt-file libconfig-dev libxcomposite-dev libxdamage-dev libxrandr-dev libxinerama-dev libdbus-1-dev libdrm-dev libmesa-dev
+sudo apt-get install libglu1-mesa-dev freeglut3-dev mesa-common-dev asciidoc
+
+
+git clone https://github.com/chjj/compton
+cd compton/
+sudo make
+sudo make docs
+sudo make install
+cd ..
+rm -rf compton/
+echo compton installed!
